9c9f528ecb2160b1b8eafa9df7256c0004d74523
annt07 <anantadeva07@gmail.com>
2025-12-02 00:11:20 +0700

    Feat: Create UI Baseline for partial components (Navbar, Footer), Created baseline for Landing page, and dashboard. Complete Auth System (Login/Register/Logout UI)

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Transaction, ChatHistory
 
 # Register your models here.
+admin.site.register(Transaction)
+admin.site.register(ChatHistory)
+
+
