f1ac07faa97b44aaad523c2affa1900001a943eb
Ananta Deva <153338891+admiralannt07@users.noreply.github.com>
2025-12-03 18:53:58 +0700

    Revert "Fix/UI fix stress test (#8)"
    This reverts commit 52dbecabd5194721d65ed52a81afddc64b286e64.

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Transaction, ChatHistory
 
 # Register your models here.
-admin.site.register(Transaction)
-admin.site.register(ChatHistory)
-
-
