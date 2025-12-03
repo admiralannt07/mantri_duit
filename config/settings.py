0726dd2a941fc0bad3f71a5c917c89f295fc2406
Ananta Deva <153338891+admiralannt07@users.noreply.github.com>
2025-12-03 18:52:08 +0700

    Revert "Fix/UI fix stress test (#8)"
    This reverts commit 52dbecabd5194721d65ed52a81afddc64b286e64.

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize',
     'core',
     'django_htmx',
 ]
@@ -123,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static", # Django cari static di folder root 'static'
 ]
@@ -136,8 +135,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Auth Settings
-LOGIN_REDIRECT_URL = 'dashboard' 
-LOGOUT_REDIRECT_URL = 'landing'  
-LOGIN_URL = 'login'              
--- config/urls.py
+++ config/urls.py
@@ -20,8 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('donttouchtheadminorelse/', admin.site.urls),
-    path('', include('core.urls')),
+    path('admin/', admin.site.urls),
 ]
 
 # For debug
